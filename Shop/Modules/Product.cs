c7bca459cbefc97aae70065119ca6ec77dc704ed
Ana Višnevskaja <anavisnia@gmail.com>
2021-06-11 10:30:49 +0300

    Add project files.

---
+++ Shop/Modules/Product.cs
@@ -0,0 +1,20 @@
+﻿using Shop.Modules.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Modules
+{
+    public class Product : Entity
+    {
+        public int Quantity { get; set; }
+
+        public int ShopId { get; set; }
+
+        public Shop Shop { get; set; }
+
+        public bool Deleted { get; set; } = false;
+    }
+}
---
+++ Shop/Modules/Shop.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Modules.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Modules
+{
+    public class Shop : Entity
+    {
+        
+    }
+}
