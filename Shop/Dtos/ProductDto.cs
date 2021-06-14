c65c9ad858011948a02dce65735cf4077c8787d7
Ana Višnevskaja <anavisnia@gmail.com>
2021-06-14 14:32:31 +0300

    Added dtos

---
+++ Shop/Dtos/ProductDto.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Dtos
+{
+    public class ProductDto : DtoObject
+    {
+        public string Type { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int ShopId { get; set; }
+    }
+}
---
+++ Shop/Dtos/ShopDto.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Dtos.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Dtos
+{
+    public class ShopDto : DtoObject
+    {
+        
+    }
+}
